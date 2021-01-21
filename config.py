27f930bc3b6c369b8608df444db220daec4658d6
aryan5610 <67310521+aryan5610@users.noreply.github.com>
2021-01-22 01:44:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,6 +20,6 @@ class Config:
 	
  TOKEN=os.environ.get("BOT_TOKEN",None)
  SOURCE="https://github.com/Artis7eeR/ForwardTagRemoverBot"
- START_TEXT="Hi [{}](tg://user?id={})\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do \n ©Artis7eeR"
+ START_TEXT="Hi [{}](tg://user?id={})\nI am for use of anime volume .. not useful for you so don't spam and block me "
  HELP_TEXT="Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n\n`How to Set Caption?`\nReply Caption to a File,Photo,Audio,Media"
 	
